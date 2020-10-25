eb1a9189e3e93371337df025b0b3b8a2dcfc7fee
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2020-10-25 00:35:08 -0400

    api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import "./normalize.css";
+import "./skeleton.css";
+//import logo from './logo.svg';
+//import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    };
-  }
+function App() {
+  const [query, setQuery] = useState("");
+  const url =
+    "https://newsapi.org/v2/everything?q=${query}&apiKey=7a24ab3bcf634857bbdd7a59206a6778";
 
-  componentDidMount() {
-    fetch(
-      "http://newsapi.org/v2/everything?domains=wsj.com&apiKey=7a24ab3bcf634857bbdd7a59206a6778"
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          isLoaded: true,
-        });
-      });
-  }
+  const getData = async () => {
+    const result = await Axios.get(url);
+    console.log(result);
+    setQuery("");
+  };
 
-  render() {
-    var { isLoaded, items } = this.state;
-    if (!isLoaded) {
-      return <div>Loading... </div>;
-    } else {
-      return (
-        <div className="App">
-          <ul>
-            {items.map((item) => (
-              <li key={item.id}>
-                Author: {item.author} | Title: {item.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    }
-  }
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+  return (
+    <div className="App">
+      <h1 onClick={getData}> News</h1>
+      <form className="search-bar" onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Search News"
+          autoComplete="off"
+          onChange={onChange}
+          value={query}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
