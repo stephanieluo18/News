63a15fa6553690206a3a24e5fa6edffd644c606b
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2020-10-29 23:37:04 -0400

    deploy site

---
+++ build/precache-manifest.e91c88ddcc3305d2d016fcb3a0244c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83d936ca749817a44fc73d4f35ed58e",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "51dee633e743afc2541f",
+    "url": "/News/static/css/main.0ca0fc52.chunk.css"
+  },
+  {
+    "revision": "04d9f716226d8ae17b93",
+    "url": "/News/static/js/2.7e6b097c.chunk.js"
+  },
+  {
+    "revision": "b286ef3fbbdc3cc278bdfc28d6375bdc",
+    "url": "/News/static/js/2.7e6b097c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dee633e743afc2541f",
+    "url": "/News/static/js/main.4da2f512.chunk.js"
+  },
+  {
+    "revision": "af8a46cb410ed1bcf809",
+    "url": "/News/static/js/runtime-main.4014c539.js"
+  }
+]);
